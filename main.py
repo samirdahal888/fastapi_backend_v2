ada0626396d6f3690f155d01a30b07ecb2eacc47
Samir Dahal <samirdahal877@gmail>
2025-07-03 12:55:09 +0545

    make better message for home route

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
-from database import create_db_and_tables
-from router import Admin,student
 
+from database import create_db_and_tables
+from router import admin, student
 
 app = FastAPI(
-   title='Student management system' ,
-   description = "simple version to keep students data in database"
-
+    title="Student management system",
+    description="simple version to keep students data in database",
 )
 
 
@@ -14,19 +13,11 @@ app = FastAPI(
 def on_startup():
     create_db_and_tables()
 
+
 @app.get("/")
 def home():
-    return{'message':"this is the home page"}
+    return {"message": "Every thing is OK"}
 
-app.include_router(Admin.router)
+
+app.include_router(admin.router)
 app.include_router(student.router)
-
-
-
-
-
-
-
-
-
-
